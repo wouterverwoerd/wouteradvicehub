412419af96385a446493e9932410e78d7c478153
wouterverwoerd <wouter.verwoerd@gmail.com>
2026-08-01 21:13:34 +1200

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ import { isMySqlConnected } from './src/server/db/sequelize.js';
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 4000;
 
   app.use(cors());
   app.use(express.json());
